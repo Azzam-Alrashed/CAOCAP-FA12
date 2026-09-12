dee9fa92cd5328997c12df3c62c078a7300fa9c9
Azzam Alrashed <azzam.rar@gmail.com>
2026-09-12 12:50:11 +0300

    Add iOS and macOS SwiftUI app scaffolds with brand assets

---
+++ Apps/iOS/caocap/caocap/caocapApp.swift
@@ -0,0 +1,17 @@
+//
+//  caocapApp.swift
+//  caocap
+//
+//  Created by Azzam Alrashed on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct caocapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
